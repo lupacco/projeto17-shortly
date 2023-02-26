173955294d85df7ad5f231025d41548ec299e2b6
lupacco <pagottolc@gmail.com>
2023-02-26 16:27:38 -0300

    Token validation Bearer format

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,9 +2,11 @@ import { db } from "../config/database.connection.js";
 
 export async function validateToken(req, res, next){
     const { authorization } = req.headers
+    const token = authorization.replace("Bearer ", "")
+
 
     try{
-        const query = await db.query(`SELECT * FROM sessions WHERE token=$1`, [authorization])
+        const query = await db.query(`SELECT * FROM sessions WHERE token=$1`, [token])
         const session = query.rows[0]
 
         if(!session) return res.sendStatus(401)
